d8c0075a346ede57f22e59a7fa5224d99125fba7
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-08-22 20:39:05 -0400

    Add conditional

--- timer1.js
+++ timer1.js
@@ -1,11 +1,13 @@
 const args = process.argv.slice(2);
 
 const timer = function(beep) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  },
-  beep * 1000);
-};
+  if (!isNaN(beep) && beep > 0) {
+    setTimeout(() => {
+       process.stdout.write('\x07');
+        },
+     beep * 1000);
+      }
+   };
 
 for (let i of args) {
   timer(i);
